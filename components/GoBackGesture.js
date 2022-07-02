454d7cccd0d547ce5eba4a0e600f9a1d6d4ba61b
Brian Choi <brian9206@hotmail.com>
2022-07-02 13:49:44 +0800

    Better swipe back UX

---
+++ components/GoBackGesture.js
@@ -0,0 +1,160 @@
+﻿import { Animated, StyleSheet, useWindowDimensions, View } from 'react-native'
+import { PanGestureHandler, State } from 'react-native-gesture-handler'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import * as Haptics from 'expo-haptics'
+import { ImpactFeedbackStyle } from 'expo-haptics'
+
+const THRESHOLD = 100
+
+export default function GoBackGesture({ children, enable, onGesture }) {
+  const touchXRef = useRef(new Animated.Value(0))
+  const startXRef = useRef(new Animated.Value(0))
+  const dotAnimRef = useRef(new Animated.Value(0))
+  const [passThreshold, setPassThreshold] = useState(false)
+
+  const { width } = useWindowDimensions()
+
+  const onGestureEvent = useCallback(
+    Animated.event([{ nativeEvent: { x: touchXRef.current } }], {
+      useNativeDriver: true,
+      listener: ({ nativeEvent }) => {
+        if (!enable) return
+
+        const pass = nativeEvent.x + startXRef.current._value >= THRESHOLD
+
+        if (pass && !passThreshold) {
+          Haptics.impactAsync(ImpactFeedbackStyle.Light).then()
+          Animated.timing(dotAnimRef.current, undefined).stop()
+          Animated.timing(dotAnimRef.current, {
+            toValue: 1,
+            duration: 100,
+            useNativeDriver: true,
+          }).start()
+        } else if (!pass && passThreshold) {
+          Animated.timing(dotAnimRef.current, undefined).stop()
+          Animated.timing(dotAnimRef.current, {
+            toValue: 0,
+            duration: 100,
+            useNativeDriver: true,
+          }).start()
+        }
+
+        setPassThreshold(pass)
+      },
+    }),
+    [passThreshold, enable]
+  )
+
+  const onHandlerStateChange = useCallback(
+    ({ nativeEvent }) => {
+      if (nativeEvent.state === State.END) {
+        Animated.timing(touchXRef.current, {
+          toValue: 0,
+          duration: 250,
+          useNativeDriver: true,
+        }).start()
+
+        Animated.timing(startXRef.current, {
+          toValue: 0,
+          duration: 250,
+          useNativeDriver: true,
+        }).start()
+
+        Animated.timing(dotAnimRef.current, {
+          toValue: 0,
+          duration: 250,
+          useNativeDriver: true,
+        }).start()
+
+        if (passThreshold) {
+          if (onGesture) onGesture()
+        }
+      } else if (nativeEvent.state === State.BEGAN) {
+        startXRef.current.setValue(-nativeEvent.x)
+      }
+    },
+    [passThreshold]
+  )
+
+  return (
+    <PanGestureHandler
+      enabled={enable}
+      onGestureEvent={onGestureEvent}
+      onHandlerStateChange={onHandlerStateChange}
+    >
+      <Animated.View style={styles.root}>
+        {children}
+        <Animated.View
+          style={[
+            styles.wrapper,
+            {
+              transform: [
+                {
+                  translateX: Animated.add(
+                    Animated.add(
+                      touchXRef.current,
+                      startXRef.current
+                    ).interpolate({
+                      inputRange: [0, THRESHOLD],
+                      outputRange: [0, THRESHOLD],
+                      extrapolate: 'clamp',
+                    }),
+                    new Animated.Value(-width)
+                  ),
+                },
+              ],
+            },
+          ]}
+        >
+          <Animated.View
+            style={[
+              styles.dot,
+              {
+                transform: [
+                  {
+                    scale: dotAnimRef.current.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [1, 2],
+                    }),
+                  },
+                ],
+              },
+            ]}
+          >
+            <Ionicons name="arrow-back" size={24} color="#fff" />
+          </Animated.View>
+        </Animated.View>
+      </Animated.View>
+    </PanGestureHandler>
+  )
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+  wrapper: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+  },
+  dot: {
+    width: 30,
+    height: 30,
+    borderRadius: 30 / 2,
+    backgroundColor: '#42a5f5',
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+})
---
+++ components/LIHKG.js
@@ -0,0 +1,238 @@
+﻿import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import * as Linking from 'expo-linking'
+import * as SplashScreen from 'expo-splash-screen'
+import { BackHandler, Platform, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import WebView from 'react-native-webview'
+import { forwardRef, useCallback, useEffect, useRef, useState } from 'react'
+import GoBackGesture from './GoBackGesture'
+
+const INJECTED_STYLES = `
+html, body {
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+}
+
+html[data-ver="desktop"], html[data-ver="desktop"] body {
+  overflow: hidden;
+}
+
+html[data-page-type="thread"][data-ver="mobile"] ._1H_MPAqZWxI0DjGT6LORkT {
+  visibility: hidden !important;
+}
+
+html[data-page-type="thread"][data-ver="mobile"] ._1DZ_Nn0pmLuOkdbW3sB_4B > span:first-child {
+  display: none !important;
+}
+`
+
+const INJECTED_JAVASCRIPT = `
+let pageType = 'other'
+
+const observer = new MutationObserver((mutationList, observer) => {
+  // remove open in app
+  const openInAppDiv = document.querySelector('.K8RRn4bHyCRrG1IjoFbTr')
+  if (openInAppDiv) openInAppDiv.remove()
+  
+  // remove fucking ads
+  document.querySelectorAll('.WAy0Ey7yZXz96vkCPtKCd, ._3t1IIUwX87Q7giYt9CBgsT, .D0MIrDaQVCROYmrlpFNJp').forEach(e => {
+    e.src = 'about:blank'   // in case of iframe
+    e.innerHTML = ''
+  })
+  
+  // remove game button as it is not working
+  document.querySelectorAll('[title="Game Center"]').forEach(e => {
+    e.title = ''
+    e.className = ''
+    e.innerHTML = ''
+  })
+  
+  // remove footer
+  document.querySelectorAll('._2DLdp-7b_R-zIetW0YAyy_').forEach(e => {
+    e.innerHTML = '<br><br>'
+    e.className = 'fucked'
+  })
+})
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+})
+
+// hook localStorage
+const localStorageSetItem = localStorage.setItem.bind(localStorage)
+localStorage.setItem = (key, value) => {
+  localStorageSetItem(key, value)
+  
+  if (key === 'modesettings') {
+    _onModeSettingsChanged()
+  }
+}
+
+function _onModeSettingsChanged() {
+  let config
+  
+  try {
+    config = JSON.parse(localStorage.modesettings)[0]
+    if (!config) return
+  } catch (err) {
+    return
+  }
+  
+  window.ReactNativeWebView.postMessage(JSON.stringify({
+    type: 'configChanged',
+    config
+  }))
+}
+
+_onModeSettingsChanged()
+
+// detect desktop version
+function _onResize() {
+  document.documentElement.setAttribute('data-ver', window.innerWidth >= 768 ? 'desktop' : 'mobile')
+}
+
+window.addEventListener('resize', () => setTimeout(_onResize, 500))
+_onResize()
+
+// custom style injection
+const style = document.createElement('style')
+style.innerText = \`${INJECTED_STYLES}\`
+document.head.appendChild(style)
+
+// detect current page
+function _onLocationChanged() {  
+  if (location.href.indexOf('/thread/') !== -1) {
+    pageType = 'thread'
+  }
+  else if (location.href.indexOf('/category/') !== -1) {
+    pageType = 'category'
+  }
+  else {
+    pageType = 'other'
+  }
+  
+  document.documentElement.setAttribute('data-page-type', pageType)
+
+  window.ReactNativeWebView.postMessage(JSON.stringify({
+    type: 'pageChanged',
+    pageType
+  }))
+}
+
+window.history.pushState = new Proxy(window.history.pushState, {
+  apply: (target, thisArg, argArray) => {
+    const retval = target.apply(thisArg, argArray)
+    _onLocationChanged()
+    return retval
+  }
+})
+
+window.history.replaceState = new Proxy(window.history.replaceState, {
+  apply: (target, thisArg, argArray) => {
+    const retval = target.apply(thisArg, argArray)
+    _onLocationChanged()
+    return retval
+  }
+})
+
+window.addEventListener('popstate', _onLocationChanged)
+`
+
+function LIHKG() {
+  const insets = useSafeAreaInsets()
+  const [darkMode, setDarkMode] = useState(false)
+  const [pageType, setPageType] = useState('other')
+  const webViewRef = useRef()
+
+  const onMessage = useCallback((e) => {
+    const event = JSON.parse(e.nativeEvent.data)
+    if (event.type === 'configChanged') {
+      setDarkMode(event.config.darkMode)
+    } else if (event.type === 'pageChanged') {
+      setPageType(event.pageType)
+    }
+  }, [])
+
+  const onShouldStartLoadWithRequest = useCallback((e) => {
+    if (
+      e.url.startsWith('https://lihkg.com/') ||
+      e.url.startsWith('https://lih.kg/') ||
+      e.url.indexOf('recaptcha') !== -1
+    )
+      return true
+
+    if (e.navigationType === 'click') {
+      Linking.openURL(e.url).then()
+      return false
+    }
+
+    return true
+  }, [])
+
+  const onLoadEnd = useCallback(() => {
+    setTimeout(() => SplashScreen.hideAsync().then(), 100)
+  }, [])
+
+  useEffect(() => {
+    async function ready() {
+      await SplashScreen.preventAutoHideAsync()
+    }
+    ready().then()
+
+    const handleBackPressed = () => {
+      if (webViewRef.current) webViewRef.current.goBack()
+    }
+
+    if (Platform.OS === 'android') {
+      BackHandler.addEventListener('hardwareBackPress', handleBackPressed)
+
+      return () => {
+        BackHandler.removeEventListener('hardwareBackPress', handleBackPressed)
+      }
+    }
+  }, [])
+
+  const onGoBackGesture = useCallback(() => {
+    if (!webViewRef.current) return
+
+    // click the back button, and close the navbar
+    webViewRef.current.goBack()
+    webViewRef.current
+      .injectJavaScript(`document.querySelector('._3DGiuOeHzi-9xAeU4GPqcb').click()
+      document.querySelector('._2avg7BNCZG5kjB9vMIsfGj').style.left = '-280px'
+      document.querySelector('._3DGiuOeHzi-9xAeU4GPqcb').style.opacity = '0'
+      document.querySelector('._1H_MPAqZWxI0DjGT6LORkT').className = '_1H_MPAqZWxI0DjGT6LORkT'`)
+  }, [])
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        paddingTop: insets.top,
+        paddingLeft: insets.left,
+        paddingRight: insets.right,
+        backgroundColor: darkMode ? '#222' : 'white',
+      }}
+    >
+      <StatusBar style={darkMode ? 'light' : 'dark'} />
+      <GoBackGesture enable={pageType === 'thread'} onGesture={onGoBackGesture}>
+        <WebView
+          style={{ flex: 1, backgroundColor: darkMode ? '#151515' : '#f6f6f6' }}
+          source={{ uri: 'https://lihkg.com' }}
+          injectedJavaScript={INJECTED_JAVASCRIPT}
+          onMessage={onMessage}
+          onLoadEnd={onLoadEnd}
+          allowsBackForwardNavigationGestures={true}
+          decelerationRate={0.995}
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
+          onShouldStartLoadWithRequest={onShouldStartLoadWithRequest}
+          autoManageStatusBarEnabled={false}
+          ref={webViewRef}
+        />
+      </GoBackGesture>
+    </View>
+  )
+}
+
+export default forwardRef(LIHKG)
